484c25c2098ccf4f62cd42850b9b5686ef371f2c
Juan Correa <juan.co171104@gmail.com>
2025-03-04 23:59:03 -0500

    Se agregó contenido en app.js usando DOM

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+const nombreElement = document.getElementById("nombre");
+const descripcionElement = document.getElementById("descripcion");
 
+nombreElement.innerHTML = "Juan Correa"; // Nombre
+descripcionElement.textContent = "Me gusta la tecnología y estoy interesado en conocer nuevas innovaciones"; //Descripción
